37ae6849971d4063182fe8767517fa03b468579f
lixiaodong <it_lixd@163.com>
2025-03-19 10:50:59 +0800

    键盘布局边距完善

--- lib/widget/keyboard_widget.dart
+++ lib/widget/keyboard_widget.dart
@@ -1,75 +1,112 @@
 import 'package:flutter/material.dart';
 
 //自定义键盘
+//Row没有android-Grid的跨行跨列展示，所以通过自定义计算来实现。
 class KeyboardWidget extends StatelessWidget {
   const KeyboardWidget({super.key});
+
+  static const double _keySpacing = 8;
 
   @override
   Widget build(BuildContext context) {
     var totalWidth = MediaQuery.of(context).size.width.toInt();
-    return Container(
-      child: Stack(
-        children: [
-          Column(
-            spacing: 8,
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(color: Colors.grey[50]),
+          padding: const EdgeInsets.symmetric(vertical: _keySpacing),
+          child: Stack(
             children: [
-              Row(
+              Column(
+                spacing: _keySpacing,
                 children: [
-                  _keySizedContainer(totalWidth, _keyButton("1", () {})),
-                  Padding(padding: const EdgeInsets.only(right: 8)),
-                  _keySizedContainer(totalWidth, _keyButton("2", () {})),
-                  Padding(padding: const EdgeInsets.only(right: 8)),
-                  _keySizedContainer(totalWidth, _keyButton("3", () {})),
-                  Padding(padding: const EdgeInsets.only(right: 8)),
-                  _keySizedContainer(totalWidth, _backspaceButton(() {})),
-                ],
-              ),
-              Row(
-                children: [
-                  _keySizedContainer(totalWidth, _keyButton("4", () {})),
-                  Padding(padding: const EdgeInsets.only(right: 8)),
-                  _keySizedContainer(totalWidth, _keyButton("5", () {})),
-                  Padding(padding: const EdgeInsets.only(right: 8)),
-                  _keySizedContainer(totalWidth, _keyButton("6", () {})),
-                ],
-              ),
-              Row(
-                children: [
-                  _keySizedContainer(totalWidth, _keyButton("7", () {})),
-                  Padding(padding: const EdgeInsets.only(right: 8)),
-                  _keySizedContainer(totalWidth, _keyButton("8", () {})),
-                  Padding(padding: const EdgeInsets.only(right: 8)),
-                  _keySizedContainer(totalWidth, _keyButton("9", () {})),
-                ],
-              ),
-              Row(
-                children: [
-                  _keySizedContainer(
-                    totalWidth,
-                    _keyButton("0", () {}),
-                    itemColumns: 2,
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: _keySpacing),
+                      ),
+                      _keySizedContainer(totalWidth, _keyButton("1", () {})),
+                      Padding(
+                        padding: const EdgeInsets.only(right: _keySpacing),
+                      ),
+                      _keySizedContainer(totalWidth, _keyButton("2", () {})),
+                      Padding(
+                        padding: const EdgeInsets.only(right: _keySpacing),
+                      ),
+                      _keySizedContainer(totalWidth, _keyButton("3", () {})),
+                      Padding(
+                        padding: const EdgeInsets.only(right: _keySpacing),
+                      ),
+                      _keySizedContainer(totalWidth, _backspaceButton(() {})),
+                    ],
                   ),
-                  Padding(padding: const EdgeInsets.only(right: 8)),
-                  _keySizedContainer(
-                    totalWidth,
-                    _keyButton(".", () {}),
-                    itemColumns: 1,
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: _keySpacing),
+                      ),
+                      _keySizedContainer(totalWidth, _keyButton("4", () {})),
+                      Padding(
+                        padding: const EdgeInsets.only(right: _keySpacing),
+                      ),
+                      _keySizedContainer(totalWidth, _keyButton("5", () {})),
+                      Padding(
+                        padding: const EdgeInsets.only(right: _keySpacing),
+                      ),
+                      _keySizedContainer(totalWidth, _keyButton("6", () {})),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: _keySpacing),
+                      ),
+                      _keySizedContainer(totalWidth, _keyButton("7", () {})),
+                      Padding(
+                        padding: const EdgeInsets.only(right: _keySpacing),
+                      ),
+                      _keySizedContainer(totalWidth, _keyButton("8", () {})),
+                      Padding(
+                        padding: const EdgeInsets.only(right: _keySpacing),
+                      ),
+                      _keySizedContainer(totalWidth, _keyButton("9", () {})),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: _keySpacing),
+                      ),
+                      _keySizedContainer(
+                        totalWidth,
+                        _keyButton("0", () {}),
+                        itemColumns: 2,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: _keySpacing),
+                      ),
+                      _keySizedContainer(
+                        totalWidth,
+                        _keyButton(".", () {}),
+                        itemColumns: 1,
+                      ),
+                    ],
                   ),
                 ],
+              ),
+              Container(
+                alignment: Alignment.topRight,
+                padding: const EdgeInsets.only(top: 48, right: _keySpacing),
+                child: _keySizedContainer(
+                  totalWidth,
+                  _keyButton("确认", () {}),
+                  height: 120 + (2 * _keySpacing),
+                ),
               ),
             ],
           ),
-          Container(
-            alignment: Alignment.topRight,
-            padding: const EdgeInsets.only(top: 48,right: 8),
-            child: _keySizedContainer(
-              totalWidth,
-              _keyButton("确认", () {}),
-              height: 120 + (2 * 8),
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 
@@ -79,6 +116,7 @@ class KeyboardWidget extends StatelessWidget {
       style: OutlinedButton.styleFrom(
         backgroundColor: Colors.white,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+        side: const BorderSide(color: Colors.transparent),
       ),
       child: Text(
         key,
@@ -95,60 +133,12 @@ class KeyboardWidget extends StatelessWidget {
     int spacing = 8,
     double height = 40,
   }) {
-    var fullWidth = totalWidth - (rowColumns * spacing);
+    var fullWidth = totalWidth - (rowColumns * spacing + spacing);
     var itemWidth = (fullWidth / rowColumns) * itemColumns;
     if (itemColumns > 1) {
       itemWidth += spacing * (itemColumns - 1);
     }
     return SizedBox(width: itemWidth, height: height, child: child);
-  }
-
-  Widget _zeroButton(
-    BuildContext context,
-    String key, {
-    required VoidCallback onClick,
-  }) {
-    var width = MediaQuery.of(context).size.width;
-    var itemWidth = width / 4 * 2;
-    return SizedBox(
-      width: itemWidth - 4,
-      height: 40,
-      child: OutlinedButton(
-        onPressed: onClick,
-        style: OutlinedButton.styleFrom(
-          backgroundColor: Colors.white,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
-        ),
-        child: Text(
-          key,
-          style: const TextStyle(fontSize: 18, color: Colors.black),
-        ),
-      ),
-    );
-  }
-
-  Widget _pointButton(
-    BuildContext context,
-    String key, {
-    required VoidCallback onClick,
-  }) {
-    var width = MediaQuery.of(context).size.width;
-    var itemWidth = width / 4;
-    return SizedBox(
-      width: itemWidth - 8,
-      height: 40,
-      child: OutlinedButton(
-        onPressed: onClick,
-        style: OutlinedButton.styleFrom(
-          backgroundColor: Colors.white,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
-        ),
-        child: Text(
-          key,
-          style: const TextStyle(fontSize: 18, color: Colors.black),
-        ),
-      ),
-    );
   }
 
   Widget _backspaceButton(VoidCallback onClick) {
@@ -157,23 +147,10 @@ class KeyboardWidget extends StatelessWidget {
       style: OutlinedButton.styleFrom(
         backgroundColor: Colors.white,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+        side: const BorderSide(color: Colors.transparent),
+        iconColor: Colors.black,
       ),
       child: Icon(Icons.backspace),
     );
-  }
-
-  Widget _confirmButton(String key, VoidCallback onClick) {
-    return OutlinedButton(
-      onPressed: onClick,
-      style: OutlinedButton.styleFrom(
-        backgroundColor: Colors.white,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
-      ),
-      child: Text(key),
-    );
-  }
-
-  Widget _placeholderButton() {
-    return Expanded(child: SizedBox(height: 40, child: Spacer()));
   }
 }
